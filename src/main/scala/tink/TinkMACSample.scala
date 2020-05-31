21687b9bcc19999121c8db4eb247bf9d17a1a085
taretmch <tomochuri25@gmail.com>
2020-05-31 22:13:01 +0900

    Remove unnecessary declaration

--- src/main/scala/tink/TinkMACSample.scala
+++ src/main/scala/tink/TinkMACSample.scala
@@ -12,9 +12,6 @@ import org.apache.commons.codec.digest.DigestUtils
 
 object TinkMACSample {
   def main(args: Array[String]): Unit = {
-    // 秘密鍵の設定
-    val secret = "secret-key"
-    val secretDigest = DigestUtils.sha256(secret)
     // MAC プリミティブの初期化
     // 全ての MAC key type を登録する
     // MacConfig.register()
@@ -24,7 +21,7 @@ object TinkMACSample {
     //   MacWrapper.register()
     // が呼び出されている。
     MacConfig.register()
-    // KeySet の生成
+    // KeysetHandle の生成
     val handle: KeysetHandle = KeysetHandle.generateNew(MacKeyTemplates.HMAC_SHA256_128BITTAG)
     // MAC プリミティブの取得
     val mac: Mac = handle.getPrimitive(classOf[Mac])
